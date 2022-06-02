edfbf41c89419a1e0b74865de4dca187a6b0c09f
pavitra pandey <66621787+pvtrpndy@users.noreply.github.com>
2022-06-02 15:35:09 +0530

    Committing files

---
+++ CodeForces/GNU C++17/1A/159158008.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<long long> vll;
+#define pb push_back
+#define rep(i,a,b) for(int i = a; i < b; i++)
+#define all(x) x.begin(),x.end()
+
+void solve(){
+    
+}
+ 
+int main(){
+    cin.tie(0); 
+    cout.tie(0); 
+    ios_base::sync_with_stdio(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif 
+
+    ll m, n, a; cin>>m>>n>>a;
+    ll ans = ceil((double)m/a)*ceil((double)n/a);
+    cout<<ans<<"\n";
+}
